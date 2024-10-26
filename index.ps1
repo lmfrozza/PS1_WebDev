9d88311909265435fd0e8c4d20d2d3008e96ee38
lmfrozza <frozzalucca@gmail.com>
2024-10-25 21:48:34 -0300

    Login/Register Menu Done | Menu WiP

--- index.ps1
+++ index.ps1
@@ -23,13 +23,14 @@ while ($running) {
         # Mapeando os endpoints
         if ($request.Url.AbsolutePath -eq "/menu") {
             if ($context.request.HttpMethod -eq "GET") {
-                if($auth){
+                if ($auth) {
                     $context.Response.StatusCode = 200
                     $context.Response.ContentType = "text/HTML"
                     $webContent = Get-Content -Path "./templates/index.html" -Encoding UTF8
                     $encodedWebContent = [Text.Encoding]::UTF8.GetBytes($webContent)
                     $context.Response.OutputStream.Write($encodedWebContent, 0, $encodedWebContent.Length)
-                }else{
+                }
+                else {
                     $context.Response.StatusCode = 302
                     $context.Response.RedirectLocation = "/login"
                     $context.Response.Close()
@@ -49,28 +50,59 @@ while ($running) {
             else {
                 $requestBody = [System.IO.StreamReader]::new($request.InputStream).ReadToEnd()
                 $formData = [System.Web.HttpUtility]::ParseQueryString($requestBody)
+                if (!($formData["Email"])) {
 
-                $user = $formData["user"]
-                $password = $formData["password"]
-                $users = Import-Csv -Path "./users.csv"
-
-                $usuario = $users | Where-Object { $_.username -eq $user }
-                Write-Host $usuario
-                if ($usuario) {
-                    Write-Host 'Usuário Existe'
-                    if ($usuario.senha -eq $password) {
-                        Write-Host 'Senha Correta!'
-                        $auth = $true
-                        Write-Host 'Autentificado: '$auth
-                        $context.Response.StatusCode = 302
-                        $context.Response.RedirectLocation = "/menu"
-                        $context.Response.Close()
+                    $user = $formData["Name_In"]
+                    $password = $formData["Password_In"]
+                    $users = Import-Csv -Path "./users.csv"
+    
+                    $usuario = $users | Where-Object { $_.username -eq $user }
+                    Write-Host $usuario
+                    if ($usuario) {
+                        Write-Host 'Usuário Existe'
+                        if ($usuario.pword -eq $password) {
+                            Write-Host 'Senha Correta!'
+                            $auth = $true
+                            Write-Host 'Autentificado: '$auth
+                            $context.Response.StatusCode = 302
+                            $context.Response.RedirectLocation = "/menu"
+                            $context.Response.Close()
+                        }
+                        else {
+                            Write-Host 'Senha incorreta'
+                        }
                     }
                     else {
-                        Write-Host 'Senha incorreta'
+                        Write-Host 'Senha Errada!'
                     }
-                }else{
-                    Write-Host 'Senha Errada!'
+                }
+                else {
+                    $user_register = $formData["Name"]
+                    $password_register = $formData["Password"]
+                    $email = $formData["Email"]
+                    
+                    $new_register = [PSCustomObject]@{
+                        username = $user_register
+                        pword    = $password_register
+                        role     = "user"
+                        email    = $email
+                    }
+
+                    $csvpath = "./users.csv"
+
+                    if (Test-Path $csvpath) {
+                        $users_data = Import-Csv -Path $csvpath
+                        $users_data = @($users_data)
+
+                        $users_data += $new_register
+    
+                        $users_data | Export-Csv -Path $csvpath -NoTypeInformation
+
+                        #Add welcome Screen
+                    }
+                    else {
+                       #Redirect to error msg
+                    }
                 }
             }
         }
